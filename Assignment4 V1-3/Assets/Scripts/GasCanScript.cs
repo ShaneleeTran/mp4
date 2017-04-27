92d492a1dbb1567e2788406a724ddfdea0ed0c45
Jayse Farrell <jafarrellpersonal@gmail.com>
2017-04-27 11:16:07 -0700

    build

---
+++ Assignment4 V1-3/Assets/Scripts/GasCanScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasCanScript : MonoBehaviour {
+
+    GlobalBehavior globalBehavior = null;
+    private GameObject Player;
+
+    // Use this for initialization
+    void Start () {
+        globalBehavior = GameObject.Find("GameManager").GetComponent<GlobalBehavior>();
+        Player = GameObject.Find("Hero");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Vector3.Distance(transform.position, Player.transform.position) < 20f)
+        {
+            Destroy(this.gameObject);
+            globalBehavior.fuel++;
+        }        
+    }
+}
---
+++ Assignment4 V1-3/Assets/Scripts/ResizeBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResizeBackground : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        float cameraWidth = cameraHeight * Screen.width / Screen.height;
+        gameObject.transform.localScale = Vector3.one * cameraHeight / 5.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
